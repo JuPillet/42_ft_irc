8963c56412320736ef5f82f964d15ceb76af94ce
Julien Pillet <jpillet@c1r2p4.42nice.fr>
2022-05-17 08:59:12 +0200

    error _sd

--- IRCErr.hpp
+++ IRCErr.hpp
@@ -4,7 +4,7 @@
 class IRCErr {
 	const std::string _err;
 	public :
-        IRCErr( const std::string &err ):_err( "Error: " + err ){}
-        ~IRCErr() {}
-        const std::string getError() const { return _err; }
+		IRCErr( const std::string &err ):_err( "Error: " + err ){}
+		~IRCErr() {}
+		const std::string getError() const { return _err; }
 };
