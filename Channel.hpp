bd7431930feba09f9b7cd2993e9a1c072de8c6a0
Julien Pillet <jpillet@c1r2p6.42nice.fr>
2022-05-23 09:18:55 +0200

    getter cli correction

--- Channel.hpp
+++ Channel.hpp
@@ -6,73 +6,67 @@ typedef std::pair< std::string, time_t >	_pairBan;
 
 class Channel
 {
-		bool					_mod; // is operator only mode enabled
-		bool					_priv;
-		bool					_secret;
-		bool					_extMsg; // bloque ou non les messages externes
-		unsigned int			_limit; // limite d utilisateurs sur le channel, 0 pour pas de limite.
-		const std::string		_name;
-		std::string				_mdp;
-		std::list<std::string>	_chanOps;
-		std::list<Client *>		_cliCrnt;
-		std::list<std::string>	_cliVo;
-		std::list< _pairBan >	_cliBan;
+		bool							_mod; // is operator only mode enabled
+		bool							_priv;
+		bool							_secret;
+		bool							_extMsg; // bloque ou non les messages externes
+		unsigned int					_limit; // limite d utilisateurs sur le channel, 0 pour pas de limite.
+		const std::string				_name;
+		std::string						_mdp;
+		std::list<std::string>			_chanOps;
+		std::list<Client *>				_cliCrnt;
+		std::list<std::string>			_cliVo;
+		std::list< _pairBan >			_cliBan;
 
 	public :
-								Channel( void ) : _mod (0), _priv (0), _secret(0), _limit (0), _extMsg(0) {}
-								Channel( std::string str ) : _name( str ), _mod (0), _priv(0), _secret(0), _limit(0), _extMsg(0) {}
-								~Channel( void ) {}
-		std::string				getName( void ) const { return _name; }
-		void					setPass ( std::string str ) { _mdp = str; }
-		std::string				getPass( void ) const { return _mdp; }
-		void					setPriv( int sw ) { _priv = sw; }
-		bool					getPriv( void ) const { return _priv;}
-		void					setMod( int sw ) { _mod = sw; }
-		bool					getMod( void ) const { return _mod; }
-		void					setSecret ( int sw ) { _secret = sw; }
-		bool					getSecret( void ) const { return _secret; }
-		void					setExt ( int sw ) { _extMsg = sw; }
-		bool					getExt( void ) const { return _extMsg; }
-		void					setLimit (unsigned int tmp) { _limit = tmp;}
-		unsigned int			getLimit ( void ) const { return _limit; }
+										Channel( void ) : _mod (0), _priv (0), _secret(0), _limit (0), _extMsg(0) {}
+										Channel( std::string str ) : _name( str ), _mod (0), _priv(0), _secret(0), _limit(0), _extMsg(0) {}
+										~Channel( void ) {}
+		std::string						getName( void ) const { return _name; }
+		void							setPass ( std::string str ) { _mdp = str; }
+		std::string						getPass( void ) const { return _mdp; }
+		void							setPriv( int sw ) { _priv = sw; }
+		bool							getPriv( void ) const { return _priv;}
+		void							setMod( int sw ) { _mod = sw; }
+		bool							getMod( void ) const { return _mod; }
+		void							setSecret ( int sw ) { _secret = sw; }
+		bool							getSecret( void ) const { return _secret; }
+		void							setExt ( int sw ) { _extMsg = sw; }
+		bool							getExt( void ) const { return _extMsg; }
+		void							setLimit (unsigned int tmp) { _limit = tmp;}
+		unsigned int					getLimit ( void ) const { return _limit; }
 
-		bool					isOps (Client *tmp) {
+		bool							isOps (Client *tmp) {
 			for ( itStr tmpIt = _chanOps.begin(); tmpIt != _chanOps.end(); ++tmpIt ) {
 				if ( *tmpIt == tmp->getUser() )
 					return (1);
 			}
 			return (0);
 		}
-		void					setOps( Client *tmp ) { _chanOps.push_back( tmp->getUser() ); }
-		std::list<std::string>	*getOps( void ) const { return &_chanOps; }
+		void							setOps( Client *tmp ) { _chanOps.push_back( tmp->getUser() ); }
+		std::list<std::string> const	*getOps( void ) const { return &_chanOps; }
 
-		bool					isCli ( Client *tmp ) {
+		bool							isCli ( Client *tmp ) {
 			for ( clientIterator tmpIt = _cliCrnt.begin(); tmpIt != _cliCrnt.end(); ++tmpIt ) {
 				if ( *tmpIt == tmp )
 					return (1);
 			}
 			return (0);
 		}
-		void					setCli( Client *tmp ) { _cliCrnt.push_back(tmp); }
-		std::list<std::string>	*getCli( void ) const { return &_cliCrnt; }
+		void							setCli( Client *tmp ) { _cliCrnt.push_back(tmp); }
+		std::list<Client *> const		*getCli( void ) const { return &_cliCrnt; }
 
-		bool					isVo (Client *tmp) {
+		bool							isVo (Client *tmp) {
 			for ( itStr tmpIt = _cliVo.begin(); tmpIt != _cliVo.end(); ++tmpIt ){
 				if ( *tmpIt == tmp->getUser() )
 					return (1);
 			}
 			return (0);
 		}
-		void					setVo(Client *tmp) { _cliVo.push_back( tmp->getUser() ); }
-		std::list<Client *>		*getCli( void ) { return &_cliVo; }
+		void							setVo(Client *tmp) { _cliVo.push_back( tmp->getUser() ); }
+		std::list<std::string>			*getVo( void ) { return &_cliVo; }
 
-
-
-
-
-
-
-		bool					isBan (Client *tmp) {
+		bool							isBan (Client *tmp) {
 			for ( itBan tmpIt = _cliBan.begin(); tmpIt != _cliBan.end(); ++tmpIt )
 			{
 				if ( tmpIt->first == tmp->getUser() )
@@ -89,8 +83,6 @@ class Channel
 			return (0);
 		}
 
-
-
 //		bool					isBan (Client *cli)
 //		{
 //			itBan banIt;
@@ -106,17 +98,10 @@ class Channel
 //			return 0;
 //		}
 
-
-
-
-
-
-
-
 		void					setBan( Client *tmp , unsigned int nb )
 		{
 			if (!isBan(tmp))
 				_cliBan.push_back( _pairBan(tmp->getUser(), std::time(nullptr) + nb ) );
 		}
-		std::list<pairBan>		*getBan( void ) { return &_cliBan; }
+		std::list<pairBan> const		*getBan( void ) const { return &_cliBan; }
 };
